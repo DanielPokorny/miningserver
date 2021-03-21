b0cebc8ed80f742b72c37978021e5fdb6c7aaf20
Daniel Pokorný <mr.pokorny.d@gmail.com>
2021-03-21 17:41:54 +0100

    initial commit

---
+++ src/main/java/cz/pokesoft/miningserver/MiningserverApplication.java
@@ -0,0 +1,13 @@
+package cz.pokesoft.miningserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiningserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/pokesoft/miningserver/MiningserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.pokesoft.miningserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiningserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
