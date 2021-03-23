9bcfdee22d509b5975a98c1d0b0b39068dd39cf2
Daniel Pokorný <mr.pokorny.d@gmail.com>
2021-03-23 16:38:21 +0100

    pokus

--- src/main/java/cz/pokesoft/miningserver/MiningserverApplication.java
+++ src/main/java/cz/pokesoft/miningserver/MiningserverApplication.java
@@ -3,6 +3,8 @@ package cz.pokesoft.miningserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//pokus
+
 @SpringBootApplication
 public class MiningserverApplication {
 
